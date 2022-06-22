f5797c972695a1b29c95a2c99f0abae4b9ec6dc4
bloomgym <leoklo@yandex.ru>
2022-06-22 16:40:54 +0300

    First release

--- js/scripts.js
+++ js/scripts.js
@@ -10,15 +10,19 @@
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
-    var navbarShrink = function () {
+    const navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
+        const logo = document.body.querySelector('#logo');
+        const logo_rgb = document.body.querySelector('#logo_rgb');
+
         if (window.scrollY === 0) {
             navbarCollapsible.classList.remove('navbar-shrink')
+            logo_rgb.classList.add('visually-hidden')
+            logo.classList.remove('visually-hidden')
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
+            logo_rgb.classList.remove('visually-hidden')
+            logo.classList.add('visually-hidden')
         }
 
     };
@@ -33,23 +37,20 @@ window.addEventListener('DOMContentLoaded', event => {
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
+            target: '#mainNav', offset: 74,
         });
-    };
+    }
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
+    // // Collapse responsive navbar when toggler is visible
+    // const navbarToggler = document.body.querySelector('.navbar-toggler');
+    // const responsiveNavItems = [].slice.call(document.querySelectorAll('#navbarResponsive .nav-link'));
+    // responsiveNavItems.map(function (responsiveNavItem) {
+    //     responsiveNavItem.addEventListener('click', () => {
+    //         if (window.getComputedStyle(navbarToggler).display !== 'none') {
+    //             navbarToggler.click();
+    //         }
+    //     });
+    // });
 
     // Activate SimpleLightbox plugin for portfolio items
     new SimpleLightbox({
